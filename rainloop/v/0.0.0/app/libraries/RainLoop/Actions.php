9635e2688fb5853201e797930cc32961a52c2ff3
lynndotconfig <lynn.config@gmail.com>
2016-01-18 21:11:28 +0800

    [add] hook filter.pre-do-login

--- rainloop/v/0.0.0/app/libraries/RainLoop/Actions.php
+++ rainloop/v/0.0.0/app/libraries/RainLoop/Actions.php
@@ -2273,6 +2273,8 @@ class Actions
 
 		$sPassword = $this->clientRsaDecryptHelper($sPassword);
 		$this->Logger()->AddSecret($sPassword);
+		$this->Plugins()
+			->RunHook('filter.pre-do-login', array($sLogin, &$sEmail, &$sPassword));
 
 		if (0 < \strlen($sEmail) && 0 < \strlen($sPassword) &&
 			$this->Config()->Get('security', 'allow_universal_login', true) &&
